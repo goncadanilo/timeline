0a8d1c83dcff8ca3ce964363f6a45a9ac5b61539
Danilo Gonçalves <gonca.danilo@gmail.com>
2025-08-14 20:50:00 -0300

    feat: implemets the zoom feature

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
 
 export function Timeline({ lanes }) {
   return (
@@ -6,7 +7,14 @@ export function Timeline({ lanes }) {
       id="timeline"
       className="bg-gray-100 rounded mt-5 cursor-grab overflow-x-auto"
     >
-      {/* <div className="flex gap-4 mt-6 mb-2 px-2 w-full">
+      <TransformWrapper
+        initialScale={1}
+        maxScale={3}
+        doubleClick={{ mode: "toggle", animationType: "linear" }}
+      >
+        <TransformComponent>
+          <div>
+            {/* <div className="flex gap-4 mt-6 mb-2 px-2 w-full">
             {months.map((month) => (
               <div
                 key={month}
@@ -16,21 +24,24 @@ export function Timeline({ lanes }) {
               </div>
             ))}
           </div> */}
-      {lanes.map((lane) => (
-        <React.Fragment key={lane[0].id}>
-          <div className="flex items-center gap-5 p-6 w-max">
-            {lane.map((item) => (
-              <div
-                key={item.id}
-                className="bg-blue-300 px-4 py-2 flex flex-col items-center rounded"
-              >
-                <h3>{item.name}</h3>
-              </div>
+            {lanes.map((lane) => (
+              <React.Fragment key={lane[0].id}>
+                <div className="flex items-center gap-5 p-6 w-max">
+                  {lane.map((item) => (
+                    <div
+                      key={item.id}
+                      className="bg-blue-300 px-4 py-2 flex flex-col items-center rounded"
+                    >
+                      <h3>{item.name}</h3>
+                    </div>
+                  ))}
+                </div>
+                <div className="border-b border-gray-200 w-full"></div>
+              </React.Fragment>
             ))}
           </div>
-          <div className="border-b border-gray-200 w-full"></div>
-        </React.Fragment>
-      ))}
+        </TransformComponent>
+      </TransformWrapper>
     </div>
   );
 }
